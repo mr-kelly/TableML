1f412066f0f8f554bf467ce31b4113a686124035
mrkelly <chepy.v@gmail.com>
2016-08-25 14:40:47 +0800

    TableML C#

---
+++ TableML/TableML/TableObject.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+namespace TableML
+{
+    /// <summary>
+    /// 自动适配number(double)和string
+    /// </summary>
+    public class TableObject : TableFile<TableObjectRow>
+    {
+        public TableObject(string content) : base(new string[] { content }) { }
+        public TableObject(string[] contents) : base(contents) { }
+        public TableObject() : base() { }
+        public TableObject(string fileFullPath, Encoding encoding) : base(fileFullPath, encoding) { }
+
+        public new static TableObject LoadFromString(params string[] content)
+        {
+            TableObject tabFile = new TableObject(content);
+            return tabFile;
+        }
+
+        public new static TableObject LoadFromFile(string fileFullPath, Encoding encoding = null)
+        {
+            return new TableObject(fileFullPath, encoding);
+
+        }
+        public override TableObjectRow FindByPrimaryKey(object primaryKey, bool throwError = true)
+        {
+            // to number double
+            if (primaryKey is short || primaryKey is int || primaryKey is long || primaryKey is decimal ||
+                primaryKey is uint || primaryKey is ulong || primaryKey is ushort ||
+                primaryKey is float || primaryKey is bool)
+            {
+                return base.FindByPrimaryKey(Convert.ChangeType(primaryKey, typeof(double)));
+
+            }
+
+            return base.FindByPrimaryKey(primaryKey, throwError);
+
+        }
+    }
+
+}
---
+++ TableML/TableMLTests/TableMLTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TableML;
+
+namespace TableMLTests
+{
+    [TestClass]
+    public class TableMLTest
+    {
+        public string TableString1 = @"Id	Value
+int	string
+1	hi
+2	f
+3	abc 
+4	temp
+";
+        public string TableString2 = @"Id	Value
+int	string
+10	hi
+20	f
+30	abc 
+40	temp
+";
+        public string TableString1Plus2 = @"Id	Value
+int	string
+1	hi
+2	f
+3	abc 
+4	temp
+10	hi
+20	f
+30	abc 
+40	temp
+";
+        [TestMethod]
+        public void TestLoad()
+        {
+            var table = TableFile.LoadFromString(TableString1, TableString2);
+            var row = table.GetByPrimaryKey("1");
+            Assert.AreEqual("hi", row["Value"]);
+        }
+
+        /// <summary>
+        /// 传入多个Table
+        /// </summary>
+        [TestMethod]
+        public void TestMultString()
+        {
+            var table = TableFile.LoadFromString(TableString1, TableString2);
+            var row = table.GetByPrimaryKey("1");
+            Assert.AreEqual("hi", row["Value"]);
+        }
+        [TestMethod]
+        public void TestMultStringTableObj()
+        {
+            var tableObj = TableObject.LoadFromString(TableString1, TableString2);
+            var objRow = tableObj.GetByPrimaryKey(10);
+            Assert.AreEqual("hi", objRow["Value"]);
+        }
+        [TestMethod]
+        public void TestWrite()
+        {
+            var tableObj = TableFile.LoadFromString(TableString1, TableString2);
+            var writer = new TableFileWriter(tableObj);
+            Assert.AreEqual(true, writer.Save("write.txt"));
+            var result = File.ReadAllText("write.txt");
+            var expect = TableString1Plus2.Replace("\r", "");
+            Assert.AreEqual(expect, result);
+
+        }
+    }
+}
