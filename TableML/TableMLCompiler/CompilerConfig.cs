db9dd522b3d172add146507f9ff25f67c58eced4
mrkelly <chepy.v@gmail.com>
2016-08-25 18:48:53 +0800

    TableML Compiler

---
+++ TableML/TableMLCompiler/CompilerConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TableMLCompiler
+{
+
+    public class CompilerConfig
+    {
+        /// <summary>
+        /// 编译后的扩展名
+        /// </summary>
+        public string ExportTabExt = ".bytes";
+        // 被认为是注释的表头
+        public string[] CommentStartsWith = { "Comment", "#" };
+
+        /// <summary>
+        /// 定义条件编译指令
+        /// </summary>
+        public string[] ConditionVars;
+
+        public CompilerConfig()
+        {
+        }
+    }
+
+}
